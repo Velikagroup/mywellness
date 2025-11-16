fb4fc92174858cc4670ecd57c3fcd72f2d5256df
Andrea Fontana <andrea.fontana@bluadv.net>
2025-11-16 19:48:29 +0000

    File changes

--- src/components/home/AppDemoFlow.jsx
+++ src/components/home/AppDemoFlow.jsx
@@ -92,7 +92,7 @@ export default function AppDemoFlow() {
         aspectRatio: isDesktop ? '4/3' : '9/16', 
         maxHeight: isDesktop ? '490px' : '420px'
       }}>
-        {/* Content container - centrato con transform */}
+        {/* Content container - centrato rispetto alla pagina */}
         <div 
           className="absolute bg-white overflow-hidden shadow-xl"
           style={{ 
@@ -938,7 +938,7 @@ export default function AppDemoFlow() {
           </AnimatePresence>
         </div>
 
-        {/* Device frame overlay - SPOSTATO di 20px a SINISTRA su desktop */}
+        {/* Device frame overlay - SPOSTATO di 10px a DESTRA su desktop */}
         <img 
           src={isDesktop 
             ? "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/68d44c626cc2c19cca9c750d/663f4143c_Ipadtrasparent.png"
@@ -946,7 +946,7 @@ export default function AppDemoFlow() {
           }
           alt="Device Frame"
           className="absolute inset-0 w-full h-full pointer-events-none"
-          style={{ left: isDesktop ? '-20px' : '0' }}
+          style={{ left: isDesktop ? '10px' : '0' }}
         />
       </div>
     </div>
