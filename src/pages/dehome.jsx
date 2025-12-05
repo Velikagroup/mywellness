4ab928af58176918d60173c9b85b92b0969a8142
Andrea Fontana <andrea.fontana@bluadv.net>
2025-12-05 19:07:40 +0000

    File changes

--- src/pages/dehome.jsx
+++ src/pages/dehome.jsx
@@ -1,6 +1,47 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { Dialog, DialogContent } from '@/components/ui/dialog';
+import { useNavigate } from 'react-router-dom';
+import { createPageUrl } from '@/utils';
+import { base44 } from "@/api/base44Client";
+import {
+  Sparkles,
+  Target,
+  Utensils,
+  Dumbbell,
+  TrendingUp,
+  Brain,
+  Camera,
+  CheckCircle,
+  ArrowRight,
+  BarChart3,
+  Zap,
+  Activity,
+  Image as ImageIcon,
+  Clock,
+  RefreshCw,
+  BrainCircuit,
+  Users,
+  Globe
+} from 'lucide-react';
+import { motion, useScroll, useTransform } from "framer-motion";
+import { LanguageProvider, useLanguage, SUPPORTED_LANGUAGES } from '@/components/i18n/LanguageContext';
+import { translations } from '@/components/i18n/translations';
+import WorkoutPreviewDemo from "../components/home/WorkoutPreviewDemo";
+import MealPlanPreviewDemo from "../components/home/MealPlanPreviewDemo";
+import PhotoAnalyzerPreviewDemo from "../components/home/PhotoAnalyzerPreviewDemo";
+import QuizPreviewDemo from "../components/home/QuizPreviewDemo";
+import DashboardPreviewDemo from "../components/home/DashboardPreviewDemo";
+import HealthScorePreviewDemo from "../components/home/HealthScorePreviewDemo";
+import ShoppingListPreviewDemo from "../components/home/ShoppingListPreviewDemo";
+import IngredientScannerPreviewDemo from "../components/home/IngredientScannerPreviewDemo";
+import PantryPreviewDemo from "../components/home/PantryPreviewDemo";
+import MealTrackingPreviewDemo from "../components/home/MealTrackingPreviewDemo";
+import ProgressPhotoPreviewDemo from "../components/home/ProgressPhotoPreviewDemo";
+import AppDemoFlow from "../components/home/AppDemoFlow";
+import SportQuizPreviewDemo from "../components/home/SportQuizPreviewDemo";
 import Home from './Home';
-import { LanguageProvider } from '@/components/i18n/LanguageContext';
 
 export default function DeHome() {
   React.useEffect(() => {
