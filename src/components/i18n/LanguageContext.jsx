8c98a7a16ed16fb647cd018853b53c8f12b1819f
Andrea Fontana <andrea.fontana@bluadv.net>
2025-12-09 17:20:28 +0000

    File changes

--- src/components/i18n/LanguageContext.jsx
+++ src/components/i18n/LanguageContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, useCallback } from 'react';
+import React, { createContext, useContext, useState, useEffect, useMemo } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { translations } from './translations';
 
