613d9c6dc3f3b4c9f381796f4f8ded6c4b110228
Andrea Fontana <andrea.fontana@bluadv.net>
2026-01-15 16:37:12 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -65,6 +65,7 @@ import ptblogarticle from './pages/ptblogarticle';
 import ptcheckout from './pages/ptcheckout';
 import ptpricing from './pages/ptpricing';
 import ptquiz from './pages/ptquiz';
+import AdminTest from './pages/AdminTest';
 import __Layout from './Layout.jsx';
 
 
@@ -136,6 +137,7 @@ export const PAGES = {
     "ptcheckout": ptcheckout,
     "ptpricing": ptpricing,
     "ptquiz": ptquiz,
+    "AdminTest": AdminTest,
 }
 
 export const pagesConfig = {
---
+++ src/pages/AdminTest.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AdminTest() {
+  return (
+    <div className="min-h-screen animated-gradient-bg">
+      {/* Pagina completamente vuota - solo sfondo animato */}
+    </div>
+  );
+}
