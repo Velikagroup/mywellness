4384ad8f5dfb3a44987d26d578ddb6dbdd67627e
Andrea Fontana <andrea.fontana@bluadv.net>
2026-01-21 20:16:29 +0000

    File changes

--- src/components/dashboard/AdvancedProgressChart.jsx
+++ src/components/dashboard/AdvancedProgressChart.jsx
@@ -10,7 +10,7 @@ import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { base44 } from "@/api/base44Client";
 import { useLanguage } from '../i18n/LanguageContext';
-import CalorieBalanceChart from './CalorieBalanceChart';
+
 import TechnicalStatsCard from './TechnicalStatsCard';
 
 const KCAL_PER_KG = 7700;
