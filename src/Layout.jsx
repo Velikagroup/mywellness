11d007cfc4f93391c4a4c6084da68ba3e78c8635
Andrea Fontana <andrea.fontana@bluadv.net>
2026-01-23 10:01:54 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -499,7 +499,7 @@ var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n
       </div>
 
       <div className="md:hidden fixed bottom-6 left-4 right-4 z-50 flex items-center gap-3" data-menu="mobile-nav">
-        <div className="flex-1 min-w-0">
+        <div style={{ width: 'calc(100% - 80px)' }}>
           <LiquidGlassNav 
             navItems={allNavItems} 
             onActionClick={handleActionClick}
