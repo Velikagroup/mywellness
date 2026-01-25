1eceeb28bc51eb20ff944d50493d7843a750b585
Andrea Fontana <andrea.fontana@bluadv.net>
2026-01-25 19:01:10 +0000

    File changes

--- src/components/navigation/LiquidGlassNav.jsx
+++ src/components/navigation/LiquidGlassNav.jsx
@@ -121,7 +121,7 @@ export default function LiquidGlassNav({ navItems, onActionClick, showActionMenu
                 `}
               >
                 <item.icon className="w-4 h-4 md:w-5 md:h-5" />
-                <span className="text-xs font-medium text-center leading-tight hidden md:block">
+                <span className="text-[10px] md:text-xs font-medium text-center leading-tight hidden md:block">
                   {item.name}
                 </span>
               </Link>
