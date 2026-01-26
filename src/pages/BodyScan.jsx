bf86aa62123aa676d6cdb054e38b6f0e3b2cdfde
Andrea Fontana <andrea.fontana@bluadv.net>
2026-01-26 14:25:21 +0000

    File changes

--- src/pages/BodyScan.jsx
+++ src/pages/BodyScan.jsx
@@ -365,6 +365,7 @@ export default function BodyScanPage() {
                         </div>
                       </motion.div>
                     )}
+                  </AnimatePresence>
                 </motion.div>
               ))}
             </AnimatePresence>
