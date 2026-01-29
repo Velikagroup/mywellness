635dd6ec9fc829fb485dbe78f4500896ab5c80f0
Andrea Fontana <andrea.fontana@bluadv.net>
2026-01-29 15:36:06 +0000

    Pulsante Ripristina testo

--- src/components/quiz/QuizContainer.jsx
+++ src/components/quiz/QuizContainer.jsx
@@ -810,7 +810,7 @@ export default function QuizContainer({ translations, language = 'it' }) {
   }
 
   return (
-    <div className="relative pt-5 mt-[50px]">
+    <div className="relative">
       {isRecalibrateFlow && (
         <button
           onClick={() => navigate(createPageUrl('Dashboard'))}
