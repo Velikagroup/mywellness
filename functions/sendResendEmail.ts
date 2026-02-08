67672fa4a0f8e547842adf1983595ab459759fec
Andrea Fontana <andrea.fontana@bluadv.net>
2026-02-08 03:54:38 +0000

    File changes

--- functions/sendResendEmail.ts
+++ functions/sendResendEmail.ts
@@ -133,7 +133,7 @@ Deno.serve(async (req) => {
             throw new Error('RESEND_API_KEY not configured');
         }
 
-        const fromEmail = template.from_email || 'info@projectmywellness.com';
+        const fromEmail = 'info@notification.projectmywellness.com';
         
         console.log('📤 Calling Resend API...');
         
