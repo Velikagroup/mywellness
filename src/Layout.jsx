36dcf8040eba19959f273e7350387cddd1e662c2
Andrea Fontana <andrea.fontana@bluadv.net>
2026-02-12 02:35:34 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -44,6 +44,27 @@ var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n
     }(window, document, 'ttq');
   }, []);
 
+  // Meta Pixel
+  React.useEffect(() => {
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    window.fbq('init', '3810520152412779');
+    window.fbq('track', 'PageView', { content_language: language });
+  }, []);
+
+  // Track PageView on route change
+  React.useEffect(() => {
+    if (window.fbq) {
+      window.fbq('track', 'PageView', { content_language: language });
+    }
+  }, [location.pathname, language]);
+
   React.useEffect(() => {
     const loadUser = async () => {
       try {
