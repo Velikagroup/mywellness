4fbd69c3fe9ecd82067da0160ae3388c0fd56b41
Andrea Fontana <andrea.fontana@bluadv.net>
2026-02-23 02:45:19 +0000

    File changes

--- src/pages/AdminCoupons.jsx
+++ src/pages/AdminCoupons.jsx
@@ -46,6 +46,7 @@ export default function AdminCoupons() {
     ]);
     setCoupons(fetchedCoupons);
     setTransactions(fetchedTransactions);
+    setUsers(fetchedUsers);
     setIsLoading(false);
   };
 
