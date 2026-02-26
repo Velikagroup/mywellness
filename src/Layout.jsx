70725aded56e73adebf8697bc2690b2e28257bd7
Andrea Fontana <andrea.fontana@bluadv.net>
2026-02-26 23:05:15 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -33,6 +33,27 @@ function LayoutContent({ children }) {
     window.scrollTo(0, 0);
   }, [location.pathname]);
 
+  // Meta Pixel
+  React.useEffect(() => {
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    window.fbq('init', '1636022610082279');
+    window.fbq('track', 'PageView');
+  }, []);
+
+  // Track Meta PageView on route change
+  React.useEffect(() => {
+    if (window.fbq) {
+      window.fbq('track', 'PageView');
+    }
+  }, [location.pathname]);
+
   // TikTok Pixel
   React.useEffect(() => {
     !function (w, d, t) {
